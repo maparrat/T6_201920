4af284b0a23a221f7c8f2db73c50ddb73ee40701
Miguel Armando Parra Tovar <ma.parrat@uniandes.edu.co>
2019-10-11 10:07:53 -0500

    test

---
+++ src/model/data_structures/Queue.java
@@ -0,0 +1,86 @@
+package model.data_structures;
+
+import java.util.Iterator;
+
+public class Queue <T> implements IQueue <T>, Iterator<T>
+{
+	private Node front;
+	private Node back;
+
+	private int numeroElementos;
+
+	public Queue()
+	{
+		numeroElementos = 0;
+	}
+
+	public void enqueue(T item) 
+	{
+		Node nuevo = new Node();
+		nuevo.asignarDato(item);
+
+		if(back == null)
+		{
+			front = nuevo;
+		}
+		else
+		{
+			back.asignarSiguiente(nuevo);
+		}
+
+		back = nuevo;
+
+		numeroElementos++;
+	}
+
+	public T dequeue()
+	{
+		if(front != null)
+		{
+			T respuesta = (T) front.darDato();
+			front = front.darSiguente();
+			numeroElementos--;
+			
+			if(numeroElementos == 0)
+			{
+				back = null;
+			}
+			
+			return respuesta;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	public int darNumeroElementos()
+	{
+		return numeroElementos;
+	}
+	
+	public Node darPrimerNodo()
+	{
+		return front;
+	}
+	
+	public Node darUltimoNodo()
+	{
+		return back;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if(darPrimerNodo() != null)
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public T next() {
+		return dequeue();
+	}
+
+
+}
--- test/test/data_structures/TestRedBlack.java
+++ test/test/data_structures/TestRedBlack.java
@@ -1,5 +1,7 @@
 package test.data_structures;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,40 +10,119 @@ import model.data_structures.RedBlackBST;
 public class TestRedBlack 
 {
 	private RedBlackBST<Integer, String> rb;
+	private RedBlackBST<Integer, String> rbn;
 	@Before
 	public void setup1()
 	{
-		
+		rb = new RedBlackBST<>();
+		rbn = new RedBlackBST<>();
+		rb.put(1, "a");
+		rb.put(2, "b");
+		rb.put(3, "c");
+		rb.put(4, "d");
+		rb.put(5, "e");
+		rb.put(6, "f");
+		rb.put(7, "h");
+		rb.put(8, "i");
+		rb.put(9, "j");
+		rb.put(10, "k");
 	}
 	@Test
 	public void constructorTest()
 	{
-		
+		assertEquals(rb.isEmpty(), false);
+		assertEquals(rb.size(), 10);
+		assertEquals(rb.height(), 3);
+		assertEquals(rb.getHeight(rb.darRaiz()), 3);
+		assertEquals(rbn.height(), -1);
+		assertTrue(rb.contains(1));
 	}
 	@Test
 	public void getTest()
 	{
-		
+		assertEquals(rb.get(1), "a");
+		assertEquals(rb.get(2), "b");
+		assertEquals(rb.get(3), "c");
+		assertEquals(rb.get(4), "d");
+		assertEquals(rb.get(5), "e");
+		assertNull(rbn.get(1));
 	}
 	@Test
 	public void putTest()
 	{
+		rbn.put(1, "a");
+		assertEquals(rbn.isEmpty(), false);
+		assertEquals(rbn.size(), 1);
 		
+		rbn.put(2, "b");
+		assertEquals(rbn.isEmpty(), false);
+		assertEquals(rbn.size(), 2);
+		
+		rb.put(11, "z");
+		assertEquals(rb.size(), 11);
+		assertEquals(rb.get(11), "z");
+		
+		rb.put(1, "x");
+		assertEquals(rb.size(), 11);
+		assertEquals(rb.get(1), "x");
 	}
 	@Test
 	public void minTest()
 	{
+		try {
+			assertNull(rbn.min());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Integer a = 1;
+		try {
+			assertEquals(rb.min(), a);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		rb.delete(1);
+		Integer b = 2;
+		try {
+			assertEquals(rb.min(), b);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 	@Test
 	public void maxTest()
 	{
+		try {
+			assertNull(rbn.max());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Integer a = 10;
+		try {
+			assertEquals(rb.max(), a);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		rb.delete(10);
+		Integer b = 9;
+		try {
+			assertEquals(rb.max(), b);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	@Test
 	public void checkTest()
 	{
-		
+		assertTrue(rb.check());
 	}
 	@Test
 	public void keysTest()
